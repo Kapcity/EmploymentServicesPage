f10985b995e3d77422e48c9fdaeaad93d41d5b69
Kapilan <Kapster03@gmail.com>
2019-02-13 18:24:03 -0500

    updated project

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,11 @@
 /* Custom JavaScript geos Here*/
+// IIFE -- Immediately Invoked Fuction Expression
+(function(){
+
+    function Start() {
+        console.log(`%c App Started...`, "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
